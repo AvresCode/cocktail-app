4348c1648ea54e127277a4ad55895db2d96602f5
Serva <serveh.saeedi@vodafoneziggo.com>
2023-01-23 16:30:41 +0100

    Applied prettier on cypress tests

--- cypress/e2e/homePage.cy.ts
+++ cypress/e2e/homePage.cy.ts
@@ -1,16 +1,16 @@
 /// <reference types="cypress" />
 
-describe("Homepage", () => {
+describe('Homepage', () => {
   beforeEach(() => {
-    cy.visit("http://localhost:3000/");
+    cy.visit('http://localhost:3000/');
   });
 
-  it("Renders homepage", () => {
-    cy.get("#top-section").should("contain", "Welcome!");
+  it('Renders homepage', () => {
+    cy.get('#top-section').should('contain', 'Welcome!');
   });
 
-  it("Navigate to the Cocktails page", () => {
+  it('Navigate to the Cocktails page', () => {
     cy.get('[href="/cocktails"]').click();
-    cy.get("h1").should("contain", "Search Cocktail ");
+    cy.get('h1').should('contain', 'Search Cocktail ');
   });
 });
