1d91d29de8d4d1ee7662685b5865053ff8180eb5
Serva <serveh.saeedi@vodafoneziggo.com>
2023-01-23 09:31:45 +0100

    Refactored cypress test

--- cypress/e2e/searchCocktail.cy.ts
+++ cypress/e2e/searchCocktail.cy.ts
@@ -1,12 +1,17 @@
 /// <reference types="cypress" />
 
-it("Search page", () => {
-  cy.visit("http://localhost:3000/cocktails");
-  cy.get(".input-field").type("rose");
-  // cy.get(".input-field").type("amaretto rose{Enter}");
-  cy.contains("Submit").click();
-  cy.get(".cocktail-container").should("have.length", 7);
-  cy.wait(2000);
-  cy.get(":nth-child(3) > :nth-child(3) > .button-card-result").click();
-  cy.get("h1").should("have.text", "Amaretto Rose");
+describe("Search page", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/cocktails");
+  });
+
+  it("Search for a cocktail and go to a cocktail detail page", () => {
+    cy.get(".input-field").type("rose");
+    // cy.get(".input-field").type("amaretto rose{Enter}");
+    cy.contains("Submit").click();
+    cy.get(".cocktail-container").should("have.length", 7);
+    cy.wait(2000);
+    cy.get(":nth-child(3) > :nth-child(3) > .button-card-result").click();
+    cy.get("h1").should("have.text", "Amaretto Rose");
+  });
 });
